4e9e96691d364e22dba0852479ee41203877788a
blueAngel0802 <blue.angel82@outlook.com>
2025-12-14 21:00:53 -0600

    Add argument handling and input loop to Program.cs
    Enhanced Program.cs to display the current time, handle command-line arguments, and prompt for user input in a loop if no arguments are provided. Added Colorful.Console package reference to the project and included a new test.txt file with sample content.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("hello blue angel");
+Console.WriteLine("The current time is: " + DateTime.Now);
+
+if(args.Length>0)
+{
+    string message = string.Join(" ", args);
+    Console.WriteLine("You passed the following arguments: " + message);
+}else
+{
+    while(Console.ReadLine() is string line && line.Length>0)
+    {
+        Console.WriteLine("Please enter some text (or press Enter to exit): ");
+        Console.WriteLine("You entered: " + line);
+    }
+}
